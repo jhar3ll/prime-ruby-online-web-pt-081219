40311f012954cdb656738bd430c6cd30d7a492b2
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-16 02:52:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ def prime?(int)
  if int < 0 || int == 0 || int == 1 
    return false 
    
-  else
-    (2..int).to_a.all? {|num| int % num != 0}
-    return true 
+ else
+  (2..int).to_a.all? {|num| int % num != 0}
+  return true 
+  
  end
 end
-end 
